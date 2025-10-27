2da32172d6aa5f1c8a5441908d90f1bfff95a631
HenryAnyiam <anyiamhenry7@gmail.com>
2025-10-27 17:19:09 +0100

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  css: {
+    lightningcss: {
+      errorRecovery: true
+    }
+  },
   plugins: [react(), tailwindcss()],
 })
