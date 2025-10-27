d1599dd97a271f8e5c15a71de01383d4636fd7ea
Henry Anyiam <anyiamhenry7@gmail.com>
2025-10-27 14:39:08 +0100

    Update url

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,9 +5,8 @@ import AdminLayout from "../layout/AdminLayout";
 function AppRoutes() {
   return (
     <Routes>
-      {/* Marketing/Intro Website Routes */}
       <Route
-        path="/"
+        path="/admin"
         element={<AdminLayout/>}
       >
         <Route index element={<Dashboard />} />
@@ -16,13 +15,7 @@ function AppRoutes() {
         <Route path="analytics" element={<div>Analytics Page</div>} />
         <Route path="settings" element={<div>Settings Page</div>} />
       </Route>
-
-      {/* Dashboard Routes - Add these later */}
-      {/* <Route path="/admin/*" element={<AdminRoutes />} /> */}
-      {/* <Route path="/client/*" element={<ClientRoutes />} /> */}
-      {/* <Route path="/vendor/*" element={<VendorRoutes />} /> */}
-
-      {/* Catch all - redirect to home */}
+      
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
