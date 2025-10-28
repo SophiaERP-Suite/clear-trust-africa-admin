348cb5d1f8fa2e1ad8cb05815dbc78cb277bb01e
Ugbenin Wisdom <osesebaga13@gmail.com>
2025-10-28 12:02:39 +0100

    Logo update

--- src/layout/BaseDashboardLayout.tsx
+++ src/layout/BaseDashboardLayout.tsx
@@ -11,10 +11,9 @@ interface NavItem {
 
 interface BaseDashboardLayoutProps {
   navItems: NavItem[];
-  title: string;
 }
 
-function BaseDashboardLayout({ navItems, title }: BaseDashboardLayoutProps) {
+function BaseDashboardLayout({ navItems}: BaseDashboardLayoutProps) {
   // const navigate = useNavigate();
 
   useEffect(() => {
@@ -44,25 +43,10 @@ function BaseDashboardLayout({ navItems, title }: BaseDashboardLayoutProps) {
               href="/admin/dashboard"
               className="flex px-5 py-4 mr-4 rtl:ml-4 rtl:mr-0 text-xl whitespace-nowrap"
             >
-              <svg
-                width="30"
-                className="text-primary-500 dark:text-primary-500 brand-logo"
-                viewBox="0 0 30 30"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M7.25333 2H22.0444L29.7244 15.2103L22.0444 28.1333H7.25333L0 15.2103L7.25333 2ZM11.2356 9.32316H18.0622L21.3334 15.2103L18.0622 20.9539H11.2356L8.10669 15.2103L11.2356 9.32316Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M23.751 30L13.2266 15.2103H21.4755L31.9999 30H23.751Z"
-                  fill="#3FF0B9"
-                />
-              </svg>
-              <h4 className="sidebar-logo ml-2">{title}</h4>
+           
+              <span className="sidebar-logo ml-2">
+                <img src="/clear-logo.png" style={{width: "110px"}} alt="logo" />
+              </span>
             </a>
           </div>
 
