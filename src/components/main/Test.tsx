eb3f792c69f2902e1a77cec370293e58da53dc8b
Emelie-Dev <godfatheremerie@gmail.com>
2025-10-28 09:47:48 +0100

    Fix: Updated Banner area of home page

---
+++ src/components/main/Test.tsx
@@ -0,0 +1,102 @@
+//   <div className="tmp-service-area tmp-section-gap">
+//             <div className="container">
+//                 <div className="row">
+//                     <div className="col-lg-12">
+//                         <div className="tmp-section-title-border text-center">
+//                             <div className="pres-line-separator-wrapper text-center mb--10">
+//                                 <div className="line-separator line-left"></div>
+//                                 <span className="subtitle">
+//                                     <span className="number">04</span>
+//                                 <span className="subtitle-text">Services</span>
+//                                 </span>
+//                                 <div className="line-separator line-right"></div>
+//                             </div>
+//                             <h2 className="title w-700 tmp-title-split">Premium Services.</h2>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className="row g-5 service-wrapper mt--10">
+//                     <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-12">
+//                         <div className="service service__style--1 bg-color-card radius text-start tmp-border-none tmponhover">
+//                             <div className="icon">
+//                                 <!-- <i className="feather-activity"></i> -->
+//                                 <img src="assets/images/services/icons/01.png" alt="service">
+//                             </div>
+//                             <div className="content">
+//                                 <h4 className="title w-600">
+//                                     <a href="service-details-white.html">Brand Building</a>
+//                                 </h4>
+//                                 <p className="description mb--0">We help businesses create a strong brand identity, enhance visibility, and build lasting customer trust.</p>
+//                                 <div className="discover-btn mt--30">
+//                                     <a className="tmp-btn round btn-small btn-border hover-icon-reverse" href="service-details-white.html">
+//                                         <span className="icon-reverse-wrapper">
+//                         <span className="btn-text">Discover services</span>
+//                                         <span className="btn-icon"><i className="feather-arrow-right"></i></span>
+//                                         <span className="btn-icon"><i className="feather-arrow-right"></i></span>
+//                                         </span>
+//                                     </a>
+//                                 </div>
+//                             </div>
+//                             <div className="shape-service-1">
+//                                 <img src="assets/images/services/shape/01.png" alt="service">
+//                             </div>
+//                         </div>
+//                     </div>
+
+//                     <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-12">
+//                         <div className="service service__style--1 bg-color-card radius text-start tmp-border-none tmponhover">
+//                             <div className="icon">
+//                                 <!-- <i className="feather-cast"></i> -->
+//                                 <img src="assets/images/services/icons/02.png" alt="service">
+//                             </div>
+//                             <div className="content">
+//                                 <h4 className="title w-600">
+//                                     <a href="service-details-white.html">Client Support</a>
+//                                 </h4>
+//                                 <p className="description mb--0">We provide dedicated assistance, resolve issues quickly, ensure smooth communication, and deliver exceptional customer.</p>
+//                                 <div className="discover-btn mt--30">
+//                                     <a className="tmp-btn round btn-small btn-border hover-icon-reverse" href="service-details-white.html">
+//                                         <span className="icon-reverse-wrapper">
+//                         <span className="btn-text">Discover services</span>
+//                                         <span className="btn-icon"><i className="feather-arrow-right"></i></span>
+//                                         <span className="btn-icon"><i className="feather-arrow-right"></i></span>
+//                                         </span>
+//                                     </a>
+//                                 </div>
+//                             </div>
+//                             <div className="shape-service-1">
+//                                 <img src="assets/images/services/shape/01.png" alt="service">
+//                             </div>
+//                         </div>
+//                     </div>
+
+//                     <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-12">
+//                         <div className="service service__style--1 bg-color-card radius text-start tmp-border-none tmponhover">
+//                             <div className="icon">
+//                                 <!-- <i className="feather-map"></i> -->
+//                                 <img src="assets/images/services/icons/03.png" alt="service">
+//                             </div>
+//                             <div className="content">
+//                                 <h4 className="title w-600">
+//                                     <a href="service-details-white.html">Data Insights</a>
+//                                 </h4>
+//                                 <p className="description mb--0">We analyze business data, find patterns, give recommendations, and support strategic decisions.</p>
+//                                 <div className="discover-btn mt--30">
+//                                     <a className="tmp-btn round btn-small btn-border hover-icon-reverse" href="service-details-white.html">
+//                                         <span className="icon-reverse-wrapper">
+//                         <span className="btn-text">Discover services</span>
+//                                         <span className="btn-icon"><i className="feather-arrow-right"></i></span>
+//                                         <span className="btn-icon"><i className="feather-arrow-right"></i></span>
+//                                         </span>
+//                                     </a>
+//                                 </div>
+//                             </div>
+//                             <div className="shape-service-1">
+//                                 <img src="assets/images/services/shape/01.png" alt="service">
+//                             </div>
+//                         </div>
+//                     </div>
+
+//                 </div>
+//             </div>
+//         </div>
