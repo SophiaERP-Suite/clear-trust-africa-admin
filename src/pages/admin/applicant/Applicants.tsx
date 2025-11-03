0cc63dc983df0da3442d8fd7345f394657956886
HenryAnyiam <anyiamhenry7@gmail.com>
2025-11-03 16:45:46 +0100

    Adjust applicant data

--- src/pages/admin/applicant/Applicants.tsx
+++ src/pages/admin/applicant/Applicants.tsx
@@ -15,11 +15,14 @@ import "../../../assets2/js/libs.min.js";
 import "../../../assets2/js/slider-tabs.js";
 import "../../../assets2/js/sweet-alert.js";
 import "../../../assets2/js/swiper-slider.js";
-import { ChevronRightIcon, Plus, Users } from "lucide-react";
+import { ChevronRightIcon, Plus, Users, UserPlus, PenSquare, Trash2 } from "lucide-react";
 import { NavLink } from "react-router-dom";
+import female from "../../../assets2/img/female_avatar.jpg";
+import male from "../../../assets2/img/male_avatar.jpg";
 
 const data = [
   {
+    image: female,
     name: "Deborah Ononokpono",
     contact: "+2349089812359",
     email: "debbie_ono@gmail.com",
@@ -29,6 +32,7 @@ const data = [
     joined: "2019-12-01"
   },
   {
+    image: male,
     name: "Tunde Adewale",
     contact: "+2348164729381",
     email: "tunde.adewale@gmail.com",
@@ -38,15 +42,17 @@ const data = [
     joined: "2020-03-15"
   },
   {
+    image: female,
     name: "Ngozi Eze",
     contact: "+2349034471209",
     email: "ngozi.eze@yahoo.com",
     address: "5, Zik Avenue, Uwani, Enugu",
     status: "inactive",
-    employee: "St. Mary’s Secondary School",
+    employee: "St. Mary's Secondary School",
     joined: "2018-06-21"
   },
   {
+    image: male,
     name: "Ibrahim Musa",
     contact: "+2347019938452",
     email: "ibrahim.musa@gmail.com",
@@ -56,6 +62,7 @@ const data = [
     joined: "2021-09-10"
   },
   {
+    image: female,
     name: "Aisha Suleiman",
     contact: "+2348057263145",
     email: "aisha.suleiman@gmail.com",
@@ -65,6 +72,7 @@ const data = [
     joined: "2019-04-27"
   },
   {
+    image: male,
     name: "Emeka Nwosu",
     contact: "+2348075326710",
     email: "emeka.nwosu@gmail.com",
@@ -74,6 +82,7 @@ const data = [
     joined: "2022-01-12"
   },
   {
+    image: female,
     name: "Blessing Akpan",
     contact: "+2348124439087",
     email: "blessing.akpan@gmail.com",
@@ -83,6 +92,7 @@ const data = [
     joined: "2020-10-05"
   },
   {
+    image: male,
     name: "David Adebayo",
     contact: "+2349061182734",
     email: "david.adebayo@gmail.com",
@@ -92,6 +102,7 @@ const data = [
     joined: "2017-08-18"
   },
   {
+    image: female,
     name: "Chinwe Okafor",
     contact: "+2348137720945",
     email: "chinwe.okafor@gmail.com",
@@ -101,6 +112,7 @@ const data = [
     joined: "2021-02-09"
   },
   {
+    image: male,
     name: "Joseph Oladipo",
     contact: "+2349056348790",
     email: "joseph.oladipo@gmail.com",
@@ -110,6 +122,7 @@ const data = [
     joined: "2019-11-30"
   },
   {
+    image: female,
     name: "Ruth Ocheme",
     contact: "+2348145623971",
     email: "ruth.ocheme@gmail.com",
@@ -246,7 +259,7 @@ function AdminApplicants() {
                                     <td className="px-6 py-4 whitespace-nowrap">
                                       <img
                                         className="w-10 h-10 mr-3 text-primary-400 bg-primary-500/10  rounded-xl"
-                                        src="/user.jpg"
+                                        src={userData.image}
                                         style={{ objectFit: "cover" }}
                                         alt="profile"
                                       />
@@ -264,7 +277,12 @@ function AdminApplicants() {
                                       { userData.email }
                                     </td>
                                     <td className="px-6 py-4 whitespace-nowrap text-secondary-600">
-                                      <span className="inline-block whitespace-nowrap px-2 py-1 text-xs text-center font-bold leading-none text-white bg-primary-500 rounded">
+                                <span className={`
+                                  ${userData.status == 'inactive' ? 'bg-danger-500' : ''}
+                                  ${userData.status == 'pending' ? 'bg-warning-500' : ''}
+                                  ${userData.status == 'active' ? 'bg-success-500' : ''}
+                                  ${userData.status == 'hold' ? 'bg-info-500' : ''}
+                                  inline-block whitespace-nowrap px-2 py-1 text-xs text-center font-bold leading-none text-white rounded`}>
                                         { userData.status }
                                       </span>
                                     </td>
@@ -285,45 +303,7 @@ function AdminApplicants() {
                                           data-tp-title="Add"
                                         >
                                           <span className="btn-inner">
-                                            <svg
-                                              width="17"
-                                              viewBox="0 0 24 24"
-                                              fill="none"
-                                              xmlns="http://www.w3.org/2000/svg"
-                                            >
-                                              <path
-                                                fill-rule="evenodd"
-                                                clip-rule="evenodd"
-                                                d="M9.87651 15.2063C6.03251 15.2063 2.74951 15.7873 2.74951 18.1153C2.74951 20.4433 6.01251 21.0453 9.87651 21.0453C13.7215 21.0453 17.0035 20.4633 17.0035 18.1363C17.0035 15.8093 13.7415 15.2063 9.87651 15.2063Z"
-                                                stroke="currentColor"
-                                                stroke-width="1.5"
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                              ></path>
-                                              <path
-                                                fill-rule="evenodd"
-                                                clip-rule="evenodd"
-                                                d="M9.8766 11.886C12.3996 11.886 14.4446 9.841 14.4446 7.318C14.4446 4.795 12.3996 2.75 9.8766 2.75C7.3546 2.75 5.3096 4.795 5.3096 7.318C5.3006 9.832 7.3306 11.877 9.8456 11.886H9.8766Z"
-                                                stroke="currentColor"
-                                                stroke-width="1.5"
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                              ></path>
-                                              <path
-                                                d="M19.2036 8.66919V12.6792"
-                                                stroke="currentColor"
-                                                stroke-width="1.5"
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                              ></path>
-                                              <path
-                                                d="M21.2497 10.6741H17.1597"
-                                                stroke="currentColor"
-                                                stroke-width="1.5"
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                              ></path>
-                                            </svg>
+                                            < UserPlus />
                                           </span>
                                         </a>
                                         <a
@@ -335,36 +315,7 @@ function AdminApplicants() {
                                           data-tp-title="Edit"
                                         >
                                           <span className="btn-inner">
-                                            <svg
-                                              width="17"
-                                              viewBox="0 0 24 24"
-                                              fill="none"
-                                              xmlns="http://www.w3.org/2000/svg"
-                                            >
-                                              <path
-                                                d="M11.4925 2.78906H7.75349C4.67849 2.78906 2.75049 4.96606 2.75049 8.04806V16.3621C2.75049 19.4441 4.66949 21.6211 7.75349 21.6211H16.5775C19.6625 21.6211 21.5815 19.4441 21.5815 16.3621V12.3341"
-                                                stroke="currentColor"
-                                                stroke-width="1.5"
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                              ></path>
-                                              <path
-                                                fill-rule="evenodd"
-                                                clip-rule="evenodd"
-                                                d="M8.82812 10.921L16.3011 3.44799C17.2321 2.51799 18.7411 2.51799 19.6721 3.44799L20.8891 4.66499C21.8201 5.59599 21.8201 7.10599 20.8891 8.03599L13.3801 15.545C12.9731 15.952 12.4211 16.181 11.8451 16.181H8.09912L8.19312 12.401C8.20712 11.845 8.43412 11.315 8.82812 10.921Z"
-                                                stroke="currentColor"
-                                                stroke-width="1.5"
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                              ></path>
-                                              <path
-                                                d="M15.1655 4.60254L19.7315 9.16854"
-                                                stroke="currentColor"
-                                                stroke-width="1.5"
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                              ></path>
-                                            </svg>
+                                            <PenSquare />
                                           </span>
                                         </a>
                                         <a
@@ -376,35 +327,7 @@ function AdminApplicants() {
                                           data-tp-title="Delet"
                                         >
                                           <span className="btn-inner">
-                                            <svg
-                                              width="17"
-                                              viewBox="0 0 24 24"
-                                              fill="none"
-                                              xmlns="http://www.w3.org/2000/svg"
-                                              stroke="currentColor"
-                                            >
-                                              <path
-                                                d="M19.3248 9.46826C19.3248 9.46826 18.7818 16.2033 18.4668 19.0403C18.3168 20.3953 17.4798 21.1893 16.1088 21.2143C13.4998 21.2613 10.8878 21.2643 8.27979 21.2093C6.96079 21.1823 6.13779 20.3783 5.99079 19.0473C5.67379 16.1853 5.13379 9.46826 5.13379 9.46826"
-                                                stroke="currentColor"
-                                                stroke-width="1.5"
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                              ></path>
-                                              <path
-                                                d="M20.708 6.23975H3.75"
-                                                stroke="currentColor"
-                                                stroke-width="1.5"
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                              ></path>
-                                              <path
-                                                d="M17.4406 6.23973C16.6556 6.23973 15.9796 5.68473 15.8256 4.91573L15.5826 3.69973C15.4326 3.13873 14.9246 2.75073 14.3456 2.75073H10.1126C9.53358 2.75073 9.02558 3.13873 8.87558 3.69973L8.63258 4.91573C8.47858 5.68473 7.80258 6.23973 7.01758 6.23973"
-                                                stroke="currentColor"
-                                                stroke-width="1.5"
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                              ></path>
-                                            </svg>
+                                            <Trash2 />
                                           </span>
                                         </a>
                                       </div>
