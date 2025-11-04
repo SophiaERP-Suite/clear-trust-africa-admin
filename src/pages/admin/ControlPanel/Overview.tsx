68543e8ca69a9c6c37df88d32e39b4cf5963f409
HenryAnyiam <anyiamhenry7@gmail.com>
2025-11-04 17:54:32 +0100

    Add control panel

--- src/pages/admin/ControlPanel/Overview.tsx
+++ src/pages/admin/ControlPanel/Overview.tsx
@@ -4,17 +4,13 @@ function Overview() {
       <h3 className="text-xl font-semibold mb-4 text-gray-800">System Overview</h3>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         <div className="bg-blue-100 text-blue-700 p-4 rounded-md">
-          <h4 className="font-semibold text-lg">Active Employees</h4>
-          <p className="text-2xl font-bold">4</p>
+          <h4 className="font-semibold text-lg">Active Employers</h4>
+          <p className="text-2xl font-bold">54</p>
         </div>
         <div className="bg-green-100 text-green-700 p-4 rounded-md">
           <h4 className="font-semibold text-lg">Permissions Set</h4>
           <p className="text-2xl font-bold">2</p>
         </div>
-        {/* <div className="bg-yellow-100 text-yellow-700 p-4 rounded-md">
-          <h4 className="font-semibold text-lg">System Logs</h4>
-          <p className="text-2xl font-bold">412</p>
-        </div> */}
       </div>
     </div>
   );
